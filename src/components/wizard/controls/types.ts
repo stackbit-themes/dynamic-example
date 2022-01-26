7944e84e0af8e1859563c583ee6a645868a8d288
Stackbit Code Editor <projects@stackbit.com>
2022-01-26 17:12:41 +0000

    types.ts: changed

--- src/components/wizard/controls/types.ts
+++ src/components/wizard/controls/types.ts
@@ -14,6 +14,7 @@ export type ControlStateBuilder = (control: WizardControlModel, newValue: Wizard
 export let controlStateBuilders: Record<string, ControlStateBuilder> = {}
 
 export function buildInitialState(control: WizardControlModel): WizardControlState {
+    console.log()
     const initialValue = controlValueInitializers[control.type](control);
     const initialStatus = controlStateBuilders[control.type](control, initialValue);
     return initialStatus;
