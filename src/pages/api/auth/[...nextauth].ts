d30a827121fcc3011fc17062629e5d45dd799dc5
Stackbit Code Editor <projects@stackbit.com>
2022-02-06 18:29:49 +0000

    [...nextauth].ts: changed

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -28,7 +28,7 @@ providers.forEach((provider) => {
 
 let nextAuthSecret = process.env.NEXTAUTH_SECRET;
 if (!nextAuthSecret) {
-    nextAuthSecret = 
+    nextAuthSecret = 'FRZZrUI2BprpVUYnDZNnjffVKEYTxwZ9VPDGQxQWcDw='
 }
 
 export default NextAuth({ 
