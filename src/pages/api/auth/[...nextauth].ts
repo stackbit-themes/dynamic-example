42c0fa8ce85122019974c09c6198306cff98f802
Stackbit Code Editor <projects@stackbit.com>
2022-02-06 18:30:12 +0000

    [...nextauth].ts: changed

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -28,7 +28,7 @@ providers.forEach((provider) => {
 
 let nextAuthSecret = process.env.NEXTAUTH_SECRET;
 if (!nextAuthSecret) {
-    console.log()
+    console.log('NEXTAUTH_SECRET ')
     nextAuthSecret = 'FRZZrUI2BprpVUYnDZNnjffVKEYTxwZ9VPDGQxQWcDw='
 }
 
