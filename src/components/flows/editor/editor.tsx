22aaa486677853bdbeeed757e4dddfc139daef68
Stackbit Code Editor <projects@stackbit.com>
2022-02-07 11:11:39 +0000

    editor.tsx: changed

--- src/components/flows/editor/editor.tsx
+++ src/components/flows/editor/editor.tsx
@@ -30,7 +30,7 @@ const WizardFlowEditor: WizardFlowComponent = ({ flow }) => {
                     )}
                 <div data-sb-field-path=".steps">
                     {steps.map((step, index) => {
-                        // TODO remove when bug of 
+                        // TODO remove when bug
                         const stepType = step.type || 'WizardStep';
                         const Component = getComponent(stepType);
 
