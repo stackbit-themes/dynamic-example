15b407ee6fbf4e5f1ce25d2c4690eb33e0fb291b
Stackbit Code Editor <projects@stackbit.com>
2022-02-07 10:12:11 +0000

    general.tsx: changed

--- src/components/pageLayouts/general.tsx
+++ src/components/pageLayouts/general.tsx
@@ -66,7 +66,7 @@ function Sections(props: { sections: BaseSectionModel[]; showAllGroupsContent: b
                     const visible =
                         showAllGroupsContent || relevantUserGroups.includes(section.userGroup);
                     if (!visible) return null;
-                    const sectionType = 
+                    const sectionType = section.type
                     const Component = getComponent(section.type) as BaseSectionComponent;
                     // Even when skipping sections, the field-path annotation points to the correct index in the sections list
                     return (
